dcc0d90c0355eaab8d03a78278fd7198d8e08274
robocon20x <luutruongduong20@gmail.com>
2021-12-24 01:15:58 +0700

    xong bước cấu hình hibernate

--- src/main/java/com/myclass/controller/HomeController.java
+++ src/main/java/com/myclass/controller/HomeController.java
@@ -1,19 +1,47 @@
 package com.myclass.controller;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.myclass.entity.Account;
+
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
-//	@Autowired
-//	SessionFactory sessionFactory;
+	@Autowired
+	SessionFactory sessionFactory;
 
 	@GetMapping("home")
 	public String index() {	
-
+		
+		Session session  = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		try {
+			
+			List<Account> list = session.createQuery("from loginaccount").list();
+			for (Account item:list) {
+				System.out.println(item.getUsername());
+				
+			}
+			transaction.commit();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			transaction.rollback();
+		}
+		finally {
+			session.clear();
+			session.close();
+		}
 		 return "home";
 	}
 	@GetMapping("video")
